874d1bb8e4ff2280890eb1bb382546cce1d9d82b
shunyue1320 <1277952981@qq.com>
2019-12-17 16:57:52 +0800

    qq小程序游戏模板

---
+++ game.js
@@ -0,0 +1,7 @@
+// import './js/libs/weapp-adapter'
+// import './js/libs/symbol'
+import './scripts/libs/BOM/index';
+import './scripts/libs/symbol/index';
+import Main from './scripts/main';
+
+let main = new Main();
